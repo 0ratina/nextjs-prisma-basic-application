345c32075ca2449983b82e952fdc1d4863dcdd13
Albin Franzén <04albfra@skola.boras.se>
2024-08-28 15:49:10 +0200

    Davod

---
+++ app/joke/[id]/page.tsx
@@ -0,0 +1,22 @@
+import { db } from "@/prisma/db";
+
+interface Props {
+    params: { id: string }
+}
+
+export default async function JokePge(props: Props) {
+    // const joke = jokes.find((joke) => joke.id.toString() === props.params.id)
+    const joke = await db.joke.findUnique({ where: { "id": Number(props.params.id) } });
+
+    if (!joke) {
+        return (<div>Joke not found</div>);
+    }
+    return (
+        <main className="flex flex-col items-center justify-center min-h-screen p-4">
+            <div className="text-center max-w-md w-full">
+                <h2 className="text-4xl font-bold mb-4">{joke.text}</h2>
+                <p className="text-xl text-gray-600">Rating: {joke.rating}</p>
+            </div>
+        </main>
+    );
+}
---
+++ prisma/mocked.ts
@@ -0,0 +1,27 @@
+import { Joke } from "@prisma/client";
+// export interface Joke {
+//     id: number,
+//     text: string,
+//     rating: number;
+// }
+
+
+export const jokes: Joke[] = [
+    {
+        id: 1,
+        text: "blabla",
+        rating: 4.2
+    },
+
+    {
+        id: 2,
+        text: "blabla",
+        rating: 4.2
+    },
+
+    {
+        id: 3,
+        text: "blabla",
+        rating: 4.2
+    }
+];
